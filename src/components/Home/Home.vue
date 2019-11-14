04c83b607a670672d5f036d1ef9a132742ffb152
VnkHO <ho.vincent.nk@gmail.com>
2019-11-14 23:10:48 +0100

    update home

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -5,11 +5,11 @@
       <div class="home__marker home__marker--pokedex"></div>
       <span class="home__beacon home__beacon--pokedex"></span>
     </router-link>
-    <router-link class="home__router home__router--game" tag="div" to="/game">
+    <!-- <router-link class="home__router home__router--game" tag="div" to="/game">
       <p class="home__router--link home__router--link--game">Game</p>
       <div class="home__marker home__marker--game"></div>
       <span class="home__beacon home__beacon--game"></span>
-    </router-link>
+    </router-link>-->
   </div>
 </template>
 
